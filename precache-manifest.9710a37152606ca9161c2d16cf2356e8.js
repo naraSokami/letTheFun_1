bc49ef820e36cda03beafa022fc9aaf1fc1036eb
Batou <derwaeldaltonbaptiste@gmail.com>
2021-12-08 16:46:08 +0100

    Updates

---
+++ precache-manifest.9710a37152606ca9161c2d16cf2356e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa3d598a8ed85960572f55ccba88452",
+    "url": "/letTheFun_1/index.html"
+  },
+  {
+    "revision": "ed8053f3dd886a55a3b1",
+    "url": "/letTheFun_1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "405757c2db7d1467628c",
+    "url": "/letTheFun_1/static/js/2.6fe329f4.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/letTheFun_1/static/js/2.6fe329f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8053f3dd886a55a3b1",
+    "url": "/letTheFun_1/static/js/main.3a79d7b6.chunk.js"
+  },
+  {
+    "revision": "c22c21a531f6bcfd10bf",
+    "url": "/letTheFun_1/static/js/runtime-main.cc8800ee.js"
+  }
+]);
